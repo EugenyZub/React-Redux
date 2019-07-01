77a7b8e425fe72e49df1e5bb1b9aee0d9f077b62
Ëugene <evgeniys1@tut.by>
2019-07-01 17:29:21 +0300

    fix

--- react-resto-app-master/src/components/menu-list/menu-list.js
+++ react-resto-app-master/src/components/menu-list/menu-list.js
@@ -10,24 +10,12 @@ import './menu-list.scss';
 
 class MenuList extends Component {
 
-    // onError = () => {
-        
-    //     this.props.menuError()
-    //     console.log(this.props.error)
-    // }
-
     componentDidMount() {
-        
         this.props.menuRequested();
         const {RestoService} = this.props;
         RestoService.getResource()
             .then(res => this.props.menuLoaded(res))
             .catch(() => this.props.menuError())
-            //this.foo.born = 0;
-    }
-
-    componentDidCatch() {
-        // this.props.menuError();
     }
 
     render() {
@@ -36,7 +24,7 @@ class MenuList extends Component {
         if (loading) {
             return <Spinner/>
         }
-        console.log(error)
+
         if (error) {
             return (
                     <ErrorBoundry>
@@ -46,7 +34,6 @@ class MenuList extends Component {
             )
         }
 
-        //const err = error && <ErrorBoundry/>;
         return (
             <ul className="menu__list">
                 {
