026ecc552271d72c88ef4acc4d6db485a53078fe
Ëugene <evgeniys1@tut.by>
2019-07-01 18:27:25 +0300

    video changes add

--- react-resto-app-master/src/components/menu-list/menu-list.js
+++ react-resto-app-master/src/components/menu-list/menu-list.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import MenuListItem from '../menu-list-item';
 import {connect} from 'react-redux';
 import WithRestoService from '../hoc';
-import {menuLoaded, menuRequested, menuError} from '../../actions';
+import {menuLoaded, menuRequested, menuError, addedToCard} from '../../actions';
 import Spinner from '../spinner';
 import ErrorBoundry from '../error-boundry/';
 
@@ -19,7 +19,7 @@ class MenuList extends Component {
     }
 
     render() {
-        const {menuItems, loading, error} = this.props;
+        const {menuItems, loading, error, addedToCard} = this.props;
 
         if (loading) {
             return <Spinner/>
@@ -38,7 +38,11 @@ class MenuList extends Component {
             <ul className="menu__list">
                 {
                     menuItems.map(menuItem => {
-                        return <MenuListItem key={menuItem.id} menuItem={menuItem}/>
+                        return <MenuListItem 
+                                    key={menuItem.id} 
+                                    menuItem={menuItem}
+                                    onAddToCard={() => addedToCard(menuItem.id)}
+                                />
                     })
                 }
             </ul>
@@ -57,7 +61,8 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = {
     menuLoaded,
     menuRequested,
-    menuError
+    menuError,
+    addedToCard
 };
 
 export default WithRestoService()(connect(mapStateToProps, mapDispatchToProps)(MenuList));
